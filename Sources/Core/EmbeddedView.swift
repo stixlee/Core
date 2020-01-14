55f994beec6191198e136d81dadf01636d5e435e
Michael Lee <michael.lee@enverus.com>
2020-01-14 12:30:19 -0700

    Added EmbeddedView to Core

---
+++ Sources/Core/EmbeddedView.swift
@@ -0,0 +1,69 @@
+//
+//  EmbeddedView.swift
+//  soshL
+//
+//  Created by Mike Lee on 1/17/19.
+//  Copyright © 2019 RedemptionCreative.com. All rights reserved.
+//
+
+import UIKit
+
+class EmbeddedView: UIView {
+
+    var nibName: String? = nil
+
+    convenience init() {
+        self.init(frame: .zero)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        _setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        _setup()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        loadViewFromNib()
+    }
+
+    func loadViewFromNib() {
+        if let nibName = nibName {
+            let view = loadViewFromNib(named: nibName, owner: self)
+            addSubview(view)
+            let _ = view.constrainToSuperview()
+        }
+    }
+
+    func setup() { }
+
+}
+
+extension EmbeddedView {
+
+    private func _setup() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        setup()
+    }
+
+    private func loadNib(named name: String, in bundle: Bundle? = nil) -> UINib {
+        let bundle = bundle ?? Bundle.main
+        return UINib(nibName: name, bundle: bundle)
+    }
+
+    private func loadView<T: UIView>(fromNib nib: UINib, owner: AnyObject? = nil) -> T {
+        return nib.instantiate(withOwner: owner, options: nil)[0] as! T
+    }
+
+    private func loadViewFromNib<T: UIView>(named name: String, in bundle: Bundle? = nil, owner: AnyObject? = nil) -> T {
+        let nib = loadNib(named: name, in: bundle)
+        let view = loadView(fromNib: nib, owner: owner)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view as! T
+    }
+
+}
